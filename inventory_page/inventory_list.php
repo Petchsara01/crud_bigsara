128a026d1946c2d8878368da6491c36f22b97c58
Petchsara01 <1166105050049@mail.rmutt.ac.th>
2026-01-10 22:44:53 +0700

    delete comment

--- inventory_page/inventory_list.php
+++ inventory_page/inventory_list.php
@@ -13,23 +13,24 @@ include '../connect.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
 
     <style>
-        th{
+        th {
             text-align: center;
         }
-        td{
+
+        td {
             vertical-align: middle;
             text-align: center;
         }
     </style>
-   
+
 </head>
 
 
-<body style="background-color:#f8f9fa;">
-    <div class="container mt-4">
+<body>
+    <div class="container mt-4 ">
         <div class="d-flex justify-content-between align-items-center mb-3">
             <h2>รายการคลังสินค้า</h2>
             <a href="inventory_form.php" class="btn btn-primary">เพิ่มรายการ</a>
@@ -52,7 +53,7 @@ include '../connect.php';
                 <?php
                 $sql = "SELECT * FROM inventory";
                 $result = mysqli_query($conn, $sql); //ส่งคำสั่ง SQL ไปที่ฐานข้อมูล
-                
+
                 if (mysqli_num_rows($result) > 0) { //นับจำนวนแถวที่ได้จากการคิวรี่ว่ามากกว่า 0 หรือไม่
                     while ($row = mysqli_fetch_assoc($result)) { //วนลูปดึงข้อมูลจากฐานข้อมูลทีละแถวแล้วเอาข้อมูลในแถวนั้นมาแสดงผล
                         echo "<tr>";
